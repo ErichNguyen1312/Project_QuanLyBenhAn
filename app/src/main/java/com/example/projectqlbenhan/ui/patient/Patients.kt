493fab3a0bd380546dc10b8874c08f966ec27c83
ErichNguyen1312 <vinhnguyenfit1312@gmail.com>
2025-12-13 08:56:15 +0700

    nothing

--- app/src/main/java/com/example/projectqlbenhan/ui/patient/Patients.kt
+++ app/src/main/java/com/example/projectqlbenhan/ui/patient/Patients.kt
@@ -151,7 +151,6 @@ class Patients : AppCompatActivity() {
     }
 
 
-    //cac ham xu ly data
 
     fun calculateAge(dob: Long): Int {
         val dobCalendar = Calendar.getInstance().apply { timeInMillis = dob }
@@ -159,7 +158,6 @@ class Patients : AppCompatActivity() {
 
         var age = today.get(Calendar.YEAR) - dobCalendar.get(Calendar.YEAR)
 
-        // Nếu chưa tới sinh nhật năm nay thì -1 tuổi
         if (today.get(Calendar.DAY_OF_YEAR) < dobCalendar.get(Calendar.DAY_OF_YEAR)) {
             age--
         }

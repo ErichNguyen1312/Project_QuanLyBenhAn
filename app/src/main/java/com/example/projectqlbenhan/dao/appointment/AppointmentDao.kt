84669fb21a88967915fa7cf219c69425db8dfb71
ErichNguyen1312 <vinhnguyenfit1312@gmail.com>
2025-12-19 20:57:28 +0700

    final round

--- app/src/main/java/com/example/projectqlbenhan/dao/appointment/AppointmentDao.kt
+++ app/src/main/java/com/example/projectqlbenhan/dao/appointment/AppointmentDao.kt
@@ -55,16 +55,6 @@ interface AppointmentDao {
 
     ): List<AppointmentWithPatient>
 
-
-//    @Query("""
-//    SELECT COUNT(*) FROM appointments
-//    WHERE appointment_date BETWEEN :start AND :end
-//""")
-//    suspend fun countTodayAppointments(
-//        start: Long,
-//        end: Long
-//    ): Int
-
     @Query("SELECT COUNT(*) FROM appointments WHERE appointment_date BETWEEN :start AND :end")
     suspend fun countTodayAppointments(start: Long, end: Long): Int
 
